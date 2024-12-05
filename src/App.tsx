91b3d75b7a3f1d18ad3c9e0a85b03ed2eedf8cae
henrique-olivier <henrique.rosa@sptech.school>
2024-12-05 13:53:51 -0300

    canditars-se pronto

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import SendMail from "./pages/SendMail";
 import Reset from "./pages/Reset";
 import Users from "./pages/Users";
 import CompleteRegister from "./pages/CompleteRegister";
+import Vacancies from "./pages/Vacancies";
 
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
       <Route path="/resetpassword" element={<Reset />}></Route>
       <Route path="/users" element={<Users />}></Route>
       <Route path="/completeregister" element={<CompleteRegister />}></Route>
+      <Route path="/vacancies" element={<Vacancies />}></Route>
     </Routes>
   );
 }
